68e688b5234617d83219fef9e471b622010fae86
Max Fenton <max@maxfenton.com>
2016-04-22 12:16:35 -0400

    Clean up quotes.

--- wpfp-your-favs-widget.php
+++ wpfp-your-favs-widget.php
@@ -6,14 +6,13 @@ if ($favorite_post_ids):
     foreach ($favorite_post_ids as $post_id) {
     	if ($c++ == $limit) break;
         $p = get_post($post_id);
-        echo "<li>";
-        echo "<a href='".get_permalink($post_id)."' title='". $p->post_title ."'>" . $p->post_title . "</a> ";
-        echo "</li>";
+        echo '<li>';
+        echo '<a href="' . get_permalink($post_id) . '" title="' . $p->post_title . '">' . $p->post_title . '</a>';
+        echo '</li>';
     }
 else:
-    echo "<li>";
-    echo "Your favorites will be here.";
-    echo "</li>";
+    echo '<li>';
+    echo 'Your favorites will be here.';
+    echo '</li>';
 endif;
 echo "</ul>";
-?>
