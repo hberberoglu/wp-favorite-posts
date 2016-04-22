f23d97628aceab170bf3f2a015dd5235e21a8928
Max Fenton <max@maxfenton.com>
2016-04-22 12:18:54 -0400

    Cleanup JS
    - declare `var` before using a variable
    - consistent spacing

--- wpfp.js
+++ wpfp.js
@@ -1,30 +1,32 @@
 jQuery(document).ready( function($) {
-    $('.wpfp-link').live('click', function() {
-        dhis = $(this);
+    $('.wpfp-link').live( 'click', function() {
+        var dhis = $(this);
         wpfp_do_js( dhis, 1 );
-        // for favorite post listing page
-        if (dhis.hasClass('remove-parent')) {
+        // For favorite post listing page
+        if ( dhis.hasClass( 'remove-parent' ) ) {
             dhis.parent("li").fadeOut();
         }
         return false;
     });
 });
 
-function wpfp_do_js( dhis, doAjax ) {
-    loadingImg = dhis.prev();
+var wpfp_do_js = function( dhis, doAjax ) {
+    var loadingImg = dhis.prev();
+    var beforeImg = dhis.prev().prev();
+    var url = document.location.href.split('#')[0];
+    var params = dhis.attr('href').replace('?', '') + '&ajax=1';
+
     loadingImg.show();
-    beforeImg = dhis.prev().prev();
     beforeImg.hide();
-    url = document.location.href.split('#')[0];
-    params = dhis.attr('href').replace('?', '') + '&ajax=1';
+
     if ( doAjax ) {
-        jQuery.get(url, params, function(data) {
+        jQuery.get( url, params, function(data) {
                 dhis.parent().html(data);
-                if(typeof wpfp_after_ajax == 'function') {
-                    wpfp_after_ajax( dhis ); // use this like a wp action.
+                if ( typeof wpfp_after_ajax == 'function' ) {
+                    wpfp_after_ajax( dhis ); // Use this like a WP action.
                 }
                 loadingImg.hide();
             }
         );
     }
-}
+};
