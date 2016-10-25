045164934c068206697183098780aa2607ac0146
Lee <github@journalxtra.com>
2016-10-26 00:56:48 +0100

    Updated to use Velocity JS as per @maxfenton
    Maxfenton's edit is in file https://github.com/maxfenton/wp-favorite-posts/blob/85e9bad723048b6f0b0b5e5ef88646e1fa52bbd9/wpfp.js

--- wpfp.js
+++ wpfp.js
@@ -1,30 +1,45 @@
 jQuery(document).ready( function($) {
-    $('.wpfp-link').live('click', function() {
-        dhis = $(this);
+    $('.wpfp-link').live( 'click', function() {
+        var dhis = $(this);
         wpfp_do_js( dhis, 1 );
-        // for favorite post listing page
-        if (dhis.hasClass('remove-parent')) {
-            dhis.parent("li").fadeOut();
+        // For favorite post listing page
+        if ( dhis.hasClass( 'remove-parent' ) ) {
+            if ( typeof $.fn.Velocity !== 'undefined' ) {
+                dhis.parent("li").velocity('fadeOut');
+            } else {
+                dhis.parent("li").fadeOut();
+            }
         }
         return false;
     });
 });
 
-function wpfp_do_js( dhis, doAjax ) {
-    loadingImg = dhis.prev();
-    loadingImg.show();
-    beforeImg = dhis.prev().prev();
-    beforeImg.hide();
-    url = document.location.href.split('#')[0];
-    params = dhis.attr('href').replace('?', '') + '&ajax=1';
+var wpfp_do_js = function( dhis, doAjax ) {
+    var loadingImg = dhis.prev();
+    var beforeImg = dhis.prev().prev();
+    var url = document.location.href.split('#')[0];
+    var params = dhis.attr('href').replace('?', '') + '&ajax=1';
+
+    if ( typeof $.fn.Velocity !== 'undefined ' ) {
+        loadingImg.velocity({ opacity: 1 }, { display: 'inline-block' })
+        beforeImg.velocity({ opacity: 0 }, { display: 'none' })
+    } else {
+        loadingImg.show();
+        beforeImg.hide();
+    }
+
     if ( doAjax ) {
-        jQuery.get(url, params, function(data) {
+        jQuery.get( url, params, function(data) {
                 dhis.parent().html(data);
-                if(typeof wpfp_after_ajax == 'function') {
-                    wpfp_after_ajax( dhis ); // use this like a wp action.
+                if ( typeof wpfp_after_ajax == 'function' ) {
+                    wpfp_after_ajax( dhis ); // Use this like a WP action.
                 }
-                loadingImg.hide();
+                if ( typeof $.fn.Velocity !== 'undefined' ) {
+                    loadingImg.velocity({ opacity: 0 }, { display: 'none' });
+                } else {
+                    loadingImg.hide();
+                }
             }
         );
     }
-}
+};
